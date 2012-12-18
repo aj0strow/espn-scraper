61878627a56b2e62a5d8303621a764c4b33a6bf0
aj0strow <alexander.ostrow@gmail.com>
2012-12-17 18:13:07 -0800

    finished the whitespace

--- test/espn_scraper_test/espn_test.rb
+++ test/espn_scraper_test/espn_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 class EspnTest < Test::Unit::TestCase
 	
   test 'espn is up' do
-  	assert ESPN.responding?
-  	assert !ESPN.down?
+    assert ESPN.responding?
+    assert !ESPN.down?
   end
 	
   test 'paths are working' do
-  	assert_equal 'http://scores.espn.go.com', ESPN.url('scores')
-  	assert_equal 'http://espn.go.com/nba/teams', ESPN.url('nba', 'teams')
+    assert_equal 'http://scores.espn.go.com', ESPN.url('scores')
+    assert_equal 'http://espn.go.com/nba/teams', ESPN.url('nba', 'teams')
   end
 	
   test 'get pages is working' do
-  	assert ESPN.get('scores')
+    assert ESPN.get('scores')
   end
 	
   test 'add dasherize to string' do
-  	assert_equal 'string-is-dashed', 'String is dashed'.dasherize
+    assert_equal 'string-is-dashed', 'String is dashed'.dasherize
   end
 
 end
--- test/espn_scraper_test/teams_test.rb
+++ test/espn_scraper_test/teams_test.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class TeamsTest < Test::Unit::TestCase
 	
   test 'scrape nba teams' do
-  	assert ESPN.get_teams_in('nba')['southeast'].include?({ name: 'Miami Heat' })
+    assert ESPN.get_teams_in('nba')['southeast'].include?({ name: 'Miami Heat' })
   end
 	
   test 'scrape nfl teams' do
-  	assert ESPN.get_teams_in('nfl')['nfc-west'].include?({ name: 'Seattle Seahawks' })
+    assert ESPN.get_teams_in('nfl')['nfc-west'].include?({ name: 'Seattle Seahawks' })
   end
 	
   test 'scrape mlb teams' do
-  	assert ESPN.get_teams_in('mlb')['al-west'].include?({ name: 'Seattle Mariners' })
+    assert ESPN.get_teams_in('mlb')['al-west'].include?({ name: 'Seattle Mariners' })
   end
 	
   test 'scrape nhl teams' do
-  	assert ESPN.get_teams_in('nhl')['central-division'].include?({ name: 'Detroit Red Wings' })
+    assert ESPN.get_teams_in('nhl')['central-division'].include?({ name: 'Detroit Red Wings' })
   end
 	
   test 'scrape ncaa football teams' do
-  	assert ESPN.get_teams_in('college-football')['pac-12'].include?({ name: 'Washington' })
+    assert ESPN.get_teams_in('college-football')['pac-12'].include?({ name: 'Washington' })
   end
 	
   test 'scrape ncaa basketball teams' do
-  	assert ESPN.get_teams_in('mens-college-basketball')['acc'].include?({ name: 'Duke' })
+    assert ESPN.get_teams_in('mens-college-basketball')['acc'].include?({ name: 'Duke' })
   end
 	
 end
