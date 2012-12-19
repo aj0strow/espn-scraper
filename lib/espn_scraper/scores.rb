5754fe1641e4608016c0207489eddd0ddbd971e9
aj0strow <alexander.ostrow@gmail.com>
2012-12-19 05:56:03 -0800

    added nba mlb nhl and nfl score support

--- lib/espn_scraper/scores.rb
+++ lib/espn_scraper/scores.rb
@@ -1,16 +1,98 @@
 module ESPN
   
-  def self.get_scores(league, date)
-    day = date.to_s.gsub(/[^\d]+/, '')
-    doc = self.get 'scores', league, "scoreboard?date=#{ day }"
-    scores = []
-    doc.css('.mod-scorebox.final-state').each do |game|
-      scores << game.css('.team').map do |team|
-        [ team.at_css('.team-name a')['href'].split('/').last,
-        team.at_css('.finalScore').content.to_i ]
-      end
-    end
-    Hash[ *scores.flatten ]
+  def self.get_scores(league, date_or_year, week=nil)
+    scores_method = ["get_#{league}_scores_on", date_or_year, week].compact
+    Scores.send *scores_method
   end
   
+  
+  
+  module Scores
+    
+    def self.get_nba_scores_on(date)
+      doc = markup_from_date 'nba', date
+      home_away_parse doc, date
+    end
+    
+    def self.get_mlb_scores_on(date)
+      doc = markup_from_date 'mlb', date
+      home_away_parse doc, date
+    end
+    
+    def self.get_nhl_scores_on(date)
+      doc = markup_from_date 'nhl', date
+      winner_loser_parse doc, date
+    end
+    
+    def self.get_nfl_scores_on(year, week)
+      doc = markup_from_year_and_week 'nfl', year, week
+      visitor_home_parse doc
+    end
+    
+    private
+    
+    # Get Document
+    
+    def self.markup_from_date(league, date)
+      day = date.to_s.gsub(/[^\d]+/, '')
+      ESPN.get 'scores', league, "scoreboard?date=#{ day }"
+    end
+    
+    def self.markup_from_year_and_week(league, year, week)
+      http_params = %W[ seasonYear=#{year} seasonType=2 weekNumber=#{week} confId=80 ]
+      ESPN.get 'scores', league, "scoreboard?#{ http_params.join('&') }"
+    end
+    
+    # Parse Document
+    
+    def self.home_away_parse(doc, date)
+      doc.css('.mod-scorebox.final-state').map do |game|
+        game_info = { 'game_date' => date }
+        ['home', 'away'].each do |home_away|
+          team = game.at_css(".#{home_away} .team-name a")['href'].split('/').last
+          game_info["#{home_away}_team"] = team
+          
+          score = game.at_css(".#{home_away} .finalScore").content.to_i
+          game_info["#{home_away}_score"] = score
+        end
+        game_info
+      end
+    end
+    
+    def self.winner_loser_parse(doc, date)
+      doc.css('.mod-scorebox-final').map do |game|
+        game_info = { 'game_date' => date }
+        
+        teams = game.css('.team-name div').map do |td| 
+          td.at_css('a')['href'].split('/').last 
+        end
+        game_info['away_team'], game_info['home_team'] = teams
+        
+        scores = game.css('.team-score').map { |td| td.at_css('span').content }
+        game_info['away_score'], game_info['home_score'] = scores
+        
+        game_info
+      end
+    end
+    
+    def self.visitor_home_parse(doc)
+      game_dates = doc.css('.games-date text()').map do |node|
+        Date.parse(node.content)
+      end
+      game_scores = []
+      doc.css('.gameDay-Container').each_with_index do |container, i|
+        container.css('.mod-nfl-scorebox.final-state').each do |game|
+          game_info = {}
+          game_info['game_date'] = game_dates[i]
+          game_info['home_team'] = game.at_css('.home .team-name a').content.dasherize
+          game_info['home_score'] = game.at_css('.home .score .final').content.to_i
+          game_info['away_team'] = game.at_css('.visitor .team-name a').content.dasherize
+          game_info['away_score'] = game.at_css('.visitor .score .final').content.to_i
+          game_scores.push game_info
+        end
+      end
+      game_scores
+    end
+    
+  end
 end
--- test/espn_scraper_test/scores_test.rb
+++ test/espn_scraper_test/scores_test.rb
@@ -2,11 +2,33 @@ require 'test_helper'
 
 class ScoresTest < Test::Unit::TestCase
   
-  test 'orlando magic had 106 poings on february 11th 2010' do
+  test 'cavaliers had 106 points scored on them february 11th 2010' do
     day = Date.parse 'Feb 11, 2010'
-    assert_equal 106, ESPN.get_scores('nba', day)['orlando-magic']
+    scores = ESPN.get_scores('nba', day)
+    assert_equal 'cleveland-cavaliers', scores.first['home_team']
+    assert_equal 106, scores.first['away_score']
+  end
+
+  test 'oakland athletics scored 2 runs at home on may 9th 2012' do
+    day = Date.parse 'May 9, 2012'
+    scores = ESPN.get_scores('mlb', day)
+    assert_equal 'oakland-athletics', scores.first['home_team']
+    assert_equal 2, scores.first['home_score']
   end
   
-  
-  
+  test 'toronto lost in washington on december 9th 2011' do
+    day = Date.parse 'Dec 9, 2011'
+    scores = ESPN.get_scores('nhl', day)
+    assert_equal 'toronto-maple-leafs', scores.first['away_team']
+    assert_equal 'washington-capitals', scores.first['home_team']
+    assert scores.first['away_score'] < scores.first['home_score']
+  end
+    
+  test 'ravens scored 23 points in week 4, 2012' do
+    week, year = 4, 2012
+    scores = ESPN.get_scores('nfl', year, week)
+    assert_equal 'ravens', scores.first['home_team']
+    assert_equal 23, scores.first['home_score']
+  end
+    
 end
